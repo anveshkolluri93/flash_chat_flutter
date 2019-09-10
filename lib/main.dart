7d907e30b1918fae83e26566f29f581e328e37f6
“anveshkumar” <anveshkolluri93@gmail.com>
2019-09-09 19:35:00 -0500

    Refactoring the code

--- lib/main.dart
+++ lib/main.dart
@@ -10,11 +10,6 @@ class FlashChat extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData.dark().copyWith(
-        textTheme: TextTheme(
-          body1: TextStyle(color: Colors.black54),
-        ),
-      ),
       initialRoute: WelcomeScreen.id,
       routes: {
         WelcomeScreen.id: (context) => WelcomeScreen(),
@@ -25,3 +20,6 @@ class FlashChat extends StatelessWidget {
     );
   }
 }
+
+//DarkTheme makes the text white
+//LightTheme makes the text dark and also makes the hintText in grey color
