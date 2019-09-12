7dae035dbf4868c2e1108a11454a8677a7fdab72
“anveshkumar” <anveshkolluri93@gmail.com>
2019-09-11 19:28:14 -0500

    Makes the HERO logos flexible to adjust the size when key board pops-up

--- lib/screens/registration_screen.dart
+++ lib/screens/registration_screen.dart
@@ -32,11 +32,14 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
             mainAxisAlignment: MainAxisAlignment.center,
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: <Widget>[
-              Hero(
-                tag: 'logo',
-                child: Container(
-                  height: 200.0,
-                  child: Image.asset('images/logo.png'),
+              Flexible(
+                //Tries to take up 200 height if not avaiable it is flexible and takes less space
+                child: Hero(
+                  tag: 'logo',
+                  child: Container(
+                    height: 200.0,
+                    child: Image.asset('images/logo.png'),
+                  ),
                 ),
               ),
               SizedBox(
